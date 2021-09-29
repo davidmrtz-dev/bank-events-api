a0503c9cdca58418ddae08f989be5d391f07a8d8
David Martinez <davidelimg1@gmail.com>
2021-09-28 20:51:15 -0500

    [chore]: Updated event model behavior trigger

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Event, type: :model do
   describe 'associations' do
     it { should belong_to(:eventable) }
   end
+
+  it {
+    should define_enum_for(:trigger).with_values(
+      %i[
+        charge_succeeded
+        charge_pending
+        charge_failed
+        purchase_created
+        purchase_updated
+        purchase_deleted
+      ]
+    )
+  }
 end
